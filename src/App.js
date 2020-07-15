8d4bc3e6dea7332b2a2132cf7553ec43df1c6830
Magnus Brantheim <m.brantheim@gmail.com>
2020-07-15 14:16:56 +0200

    Style fix

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import "./App.css";
 import axios from "axios";
 import { Button } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import Checkbox from "@material-ui/core/Checkbox";
 
 function App() {
-  const [nrOfSimulations, setNrOfSimulations] = useState(999);
+  const [state, setState] = useState({
+    nrOfSimulations: 999,
+    switchDoors: true,
+  });
   const handleRunSimulation = () => {
     axios
       .get("/montyhall", {
         params: {
-          nrOfSimulations,
+          nrOfSimulations: state.nrOfSimulations,
         },
       })
       .then(function (response) {
@@ -27,26 +33,43 @@ function App() {
   };
 
   const handleNrOfSimulationsChange = (event) => {
-    setNrOfSimulations(event.target.value);
+    setState({ ...state, nrOfSimulations: event.target.value });
   };
 
   return (
     <div className="App">
       <header className="App-header">Monty Hall Simulator</header>
       <div className="App-content">
-        <Button variant="contained" onClick={handleRunSimulation}>
-          Run simulation
-        </Button>
         <TextField
           id="nrOfSimulations"
-          value={nrOfSimulations}
-          label="Number"
+          value={state.nrOfSimulations}
+          label="Number of simulations"
           type="number"
           onChange={handleNrOfSimulationsChange}
           InputLabelProps={{
             shrink: true,
           }}
+          style={{ width: 160 }}
         />
+        <FormGroup row>
+          <div id="App-switchDoorWrapper">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={state.switchDoors}
+                  // onChange={handleChange}
+                  name="checkedB"
+                  color="primary"
+                />
+              }
+              label="Switch doors"
+            />
+          </div>
+        </FormGroup>
+
+        <Button variant="contained" onClick={handleRunSimulation}>
+          Run simulation
+        </Button>
       </div>
     </div>
   );
